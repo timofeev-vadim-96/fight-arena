cfb9141d6036999abd2ed68308b8ea5d91f3e695
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-11-21 21:08:33 +0300

    delete docs

--- src/main/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.model","l":"FightClub"},{"p":"org.example","l":"Main"},{"p":"org.example.model","l":"Monster"},{"p":"org.example.factory","l":"MonsterFactory"}];updateSearchResults();
